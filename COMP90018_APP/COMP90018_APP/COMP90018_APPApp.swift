49b854f331f8944d7cf45fd7d274ef4748f6a110
frankwan41 <yicongw@student.unimelb.edu.au>
2023-09-07 12:16:25 +1000

    Initial IOS application commit

---
+++ COMP90018_APP/COMP90018_APP/COMP90018_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMP90018_APPApp.swift
+//  COMP90018_APP
+//
+//  Created by Yc W on 7/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct COMP90018_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
