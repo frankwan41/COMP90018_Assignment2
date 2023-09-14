213c97f48cafe4af672c43eed6ad9520f22af2e7
junranLin <junran@student.unimelb.edu.au>
2023-09-14 19:31:26 +1000

    Complete Post and user models

---
+++ COMP90018_APP/COMP90018_APP/Models/UserModel.swift
@@ -0,0 +1,26 @@
+//
+//  UserModel.swift
+//  COMP90018_APP
+//
+//  Created by Junran Lin on 14/9/2023.
+//
+
+import Foundation
+import Firebase
+
+struct User: Decodable, Identifiable{
+    var id: String
+    var userName: String
+    var gender: String
+    var email: String
+    var profileImageURL: String
+    
+    init(data: [String: Any]){
+        self.id = data["useruid"] as? String ?? ""
+        self.userName = data["username"] as? String ?? ""
+        self.gender = data["gender"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.profileImageURL = data["profileimageurl"] as? String ?? ""
+    }
+    
+}
