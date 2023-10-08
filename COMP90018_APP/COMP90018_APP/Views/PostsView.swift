c7f6ef2d2de8d3d80e12832c96c15df9a66b2425
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2023-10-08 12:45:17 +1100

    Implement logic of like button

--- COMP90018_APP/COMP90018_APP/Views/PostsView.swift
+++ COMP90018_APP/COMP90018_APP/Views/PostsView.swift
@@ -11,6 +11,10 @@ struct PostsView: View {
 //    @State var shakeResult: String  = ""
     @State private var searchCategory: String = ""
     
+    @State private var likeStates: [Bool] = Array(repeating: false, count: 20)
+    @State private var heartScale: CGFloat = 1.0
+    @State private var numLikeStates: [Int] = Array(repeating: 32, count: 20)
+    
     @AppStorage("viewDisplay") var viewSwitcher = viewPage.welcome
     @AppStorage("shakeResult") var shakeResult = ""
     
@@ -21,24 +25,26 @@ struct PostsView: View {
                 Text("Here is your shake result: \(shakeResult)")
                 ForEach(1..<20) { index in
                     ZStack {
-                        VStack(alignment:.leading){
+                        VStack(alignment:.leading, spacing: 10){
                             Image(systemName: "photo")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .font(.largeTitle)
                             Text("This is the title of the post").font(.headline)
-                            HStack{
+                            HStack(spacing: 4){
                                 Image(systemName: "person.circle")
                                 Text("User name").font(.subheadline)
                                 Spacer()
-                                Image(systemName: "heart")
-                                Text("No. Likes \(32)").font(.subheadline)
+                                LikeButton(index: index, likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
+                                Text("\(numLikeStates[index])").font(.subheadline)
                             }
                         }
                         .padding()
                         NavigationLink(destination: SinglePostView().navigationBarBackButtonHidden(true)) {
                             EmptyView()
-                        }.opacity(0)  // Making the NavigationLink invisible
+                        }
+                        .opacity(0)  // Making the NavigationLink invisible
+                        .allowsHitTesting(false)
                     }
                     
                 }
@@ -58,8 +64,49 @@ struct PostsView: View {
     }
 }
 
-//struct PostsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PostsView()
-//    }
-//}
+// MARK: COMPONENTS
+
+struct LikeButton: View {
+    let index: Int
+    @Binding var likeStates: [Bool]
+    @Binding var heartScale: CGFloat
+    @Binding var numLikeStates: [Int]
+    
+    var body: some View{
+        Button {
+            withAnimation {
+                // Slightly increase the size for a moment
+                heartScale = 1.5
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                withAnimation {
+                    heartScale = 1.0 // Return to normal size
+                }
+            }
+            toggleLikes()
+            
+        } label: {
+            Image(systemName: likeStates[index] ? "heart.fill" : "heart")
+                .scaleEffect(heartScale)
+                .foregroundColor(likeStates[index] ? .red : .gray)
+            
+        }.buttonStyle(PlainButtonStyle())
+    }
+    
+    // Toggle number of likes, +1 / -1
+    func toggleLikes(){
+        likeStates[index].toggle()
+        if likeStates[index] {
+            numLikeStates[index] += 1
+        }else{
+            numLikeStates[index] -= 1
+        }
+    }
+}
+
+
+struct PostsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostsView()
+    }
+}
--- COMP90018_APP/COMP90018_APP/Views/SinglePostView.swift
+++ COMP90018_APP/COMP90018_APP/Views/SinglePostView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct SinglePostView: View {
     
+    @State private var likeStates: [Bool] = Array(repeating: false, count: 5)
+    @State private var heartScale: CGFloat = 1.0
+    @State private var numLikeStates: [Int] = Array(repeating: 32, count: 20)
+
+    
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         NavigationView {
             ScrollView{
-                VStack{
+                VStack(spacing: 20){
                     Image(systemName: "photo")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
@@ -23,7 +28,17 @@ struct SinglePostView: View {
                         .font(.title)
                         .fontWeight(.bold)
                     Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam. Ut lectus arcu bibendum at varius vel pharetra. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Sed odio morbi quis commodo odio. Pharetra convallis posuere morbi leo urna molestie at. Nisl tincidunt eget nullam non nisi est. Nibh praesent tristique magna sit amet. Sed faucibus turpis in eu mi bibendum neque egestas congue. In arcu cursus euismod quis viverra nibh cras. Tincidunt praesent semper feugiat nibh sed. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Tristique magna sit amet purus.")
+                    HStack{
+                        Text("TimeStamp")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                    Divider()
+                    
+                    CommentsSection(likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
                 }
+                .padding(.horizontal)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
@@ -31,13 +46,32 @@ struct SinglePostView: View {
                         presentationMode.wrappedValue.dismiss()
                     } label: {
                         Image(systemName: "chevron.left")
+                            .foregroundColor(.black)
                     }
                 }
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button {
                         // Access user profile
                     } label: {
-                        Image(systemName: "person.circle.fill")
+                        HStack{
+                            Image(systemName: "person.circle.fill")
+                            Text("Username")
+                        }
+                        .foregroundColor(.black)
+                        
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        // Follow
+                    } label: {
+                        Text("Follow")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.pink)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .background(RoundedRectangle(cornerRadius: 20).stroke(Color.pink))
                     }
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -45,6 +79,7 @@ struct SinglePostView: View {
                         // Share / Other manipulations
                     }label: {
                         Image(systemName: "square.and.arrow.up")
+                            .foregroundColor(.black)
                     }
                 }
             }
@@ -52,6 +87,70 @@ struct SinglePostView: View {
         
     }
 }
+
+
+// MARK: COMPONENTS
+struct CommentsSection: View {
+    @Binding var likeStates: [Bool]
+    @Binding var heartScale: CGFloat
+    @Binding var numLikeStates: [Int]
+    
+    var body: some View {
+        VStack(alignment:.leading){
+            HStack{
+                Text("384 Comments")
+                    .font(.headline)
+                    .fontWeight(.thin)
+                Spacer()
+            }.padding(.bottom)
+            
+            ForEach(1..<5) { index in
+                SingleComment(index: index, likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
+                    .padding(.vertical)
+                Divider()
+            }
+            
+        }
+    }
+}
+
+struct SingleComment: View {
+    let index: Int
+    @Binding var likeStates: [Bool]
+    @Binding var heartScale: CGFloat
+    @Binding var numLikeStates: [Int]
+    
+    var body: some View {
+
+        HStack(alignment: .top, spacing: 10){
+            // Front section: contains only user profile photo
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 35, height: 35)
+            
+            // Middle section: contains username, comments, possible image comment
+            VStack(alignment:.leading, spacing: 5){
+                Text("Username")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("Comment")
+                Image(systemName: "photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            }
+            // End section: contains like button and number of likes
+            VStack{
+                LikeButton(index: index, likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
+                Text("\(numLikeStates[index])")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+            
+        }
+    }
+}
+
+
 
 struct SinglePostView_Previews: PreviewProvider {
     static var previews: some View {
