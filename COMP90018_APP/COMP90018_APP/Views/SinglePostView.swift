ffa468864996e4cb416e1d694843807ccc2d3b1a
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2023-10-10 10:35:33 +1100

    Add bottom bar and comment text editor

--- COMP90018_APP/COMP90018_APP/Views/SinglePostView.swift
+++ COMP90018_APP/COMP90018_APP/Views/SinglePostView.swift
@@ -9,90 +9,287 @@ import SwiftUI
 
 struct SinglePostView: View {
     
-    @State private var likeStates: [Bool] = Array(repeating: false, count: 5)
+    @State private var postLikeState = false
+    @State private var postNumLikeState =  32
+    
+    @State private var postNumComments = 6
+    @State private var isTextFieldVisible = false
+    @FocusState private var autoFocused: Bool
+    @State private var commentText: String = ""
+    
+    
+    
+    @State private var commentLikeStates: [Bool] = Array(repeating: false, count: 5)
     @State private var heartScale: CGFloat = 1.0
-    @State private var numLikeStates: [Int] = Array(repeating: 32, count: 20)
+    @State private var commentNumLikeStates: [Int] = Array(repeating: 32, count: 20)
     @State private var selectedPhotoIndex = 0
     
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         NavigationView {
-            ScrollView{
-                VStack(alignment: .leading, spacing: 20){
-                    PostPhotoView(selectedPhotoIndex: $selectedPhotoIndex)
-                    Text("Titles")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding(.horizontal)
-                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam. Ut lectus arcu bibendum at varius vel pharetra. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Sed odio morbi quis commodo odio. Pharetra convallis posuere morbi leo urna molestie at. Nisl tincidunt eget nullam non nisi est. Nibh praesent tristique magna sit amet. Sed faucibus turpis in eu mi bibendum neque egestas congue. In arcu cursus euismod quis viverra nibh cras. Tincidunt praesent semper feugiat nibh sed. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Tristique magna sit amet purus.")
-                        .padding(.horizontal)
-                    HStack{
-                        Text("TimeStamp")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        Spacer()
-                    }
-                    Divider()
-                    
-                    CommentsSection(likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
-                }
-                .padding(.horizontal)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Image(systemName: "chevron.left")
-                            .foregroundColor(.black)
-                    }
-                }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        // Access user profile
-                    } label: {
-                        HStack{
-                            Image(systemName: "person.circle.fill")
-                            Text("Username")
-                        }
-                        .foregroundColor(.black)
-                        
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        // Follow
-                    } label: {
-                        Text("Follow")
-                            .font(.subheadline)
+            ZStack {
+                // Create a background gray effect when text editor for comment is visible
+                Color.gray.opacity(isTextFieldVisible ? 0.5 : 0) // Background color
+                                .edgesIgnoringSafeArea(.all)
+                
+                ScrollView{
+                    VStack(alignment: .leading, spacing: 20){
+                        PostPhotoView(selectedPhotoIndex: $selectedPhotoIndex)
+                        Text("Titles")
+                            .font(.title)
                             .fontWeight(.bold)
-                            .foregroundColor(Color.pink)
-                            .padding(.horizontal, 15)
-                            .padding(.vertical, 5)
-                            .background(RoundedRectangle(cornerRadius: 20).stroke(Color.pink))
+                            .padding(.horizontal)
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam. Ut lectus arcu bibendum at varius vel pharetra. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Sed odio morbi quis commodo odio. Pharetra convallis posuere morbi leo urna molestie at. Nisl tincidunt eget nullam non nisi est. Nibh praesent tristique magna sit amet. Sed faucibus turpis in eu mi bibendum neque egestas congue. In arcu cursus euismod quis viverra nibh cras. Tincidunt praesent semper feugiat nibh sed. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Tristique magna sit amet purus.")
+                            .padding(.horizontal)
+                        HStack{
+                            Text("TimeStamp")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .padding(.horizontal)
+                            Spacer()
+                        }
+                        Divider()
+                        
+                        CommentsSection(commentLikeStates: $commentLikeStates, heartScale: $heartScale, commentNumLikeStates: $commentNumLikeStates)
                     }
+                    .padding(.horizontal)
                 }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button{
-                        // Share / Other manipulations
-                    }label: {
-                        Image(systemName: "square.and.arrow.up")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Image(systemName: "chevron.left")
+                                .foregroundColor(.black)
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            // Access user profile
+                        } label: {
+                            HStack{
+                                Image(systemName: "person.circle.fill")
+                                Text("Username")
+                            }
                             .foregroundColor(.black)
+                            
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            // Follow
+                        } label: {
+                            Text("Follow")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.pink)
+                                .padding(.horizontal, 15)
+                                .padding(.vertical, 5)
+                                .background(RoundedRectangle(cornerRadius: 20).stroke(Color.pink))
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button{
+                            // Share / Other manipulations
+                        }label: {
+                            Image(systemName: "square.and.arrow.up")
+                                .foregroundColor(.black)
+                        }
+                    }
+            }
+                
+                // Disable the text editor when tap screen
+                if (isTextFieldVisible) {
+                    Button(action: {
+                        autoFocused = false
+                        isTextFieldVisible = false
+                    }){
+                        Color.clear.ignoresSafeArea()
                     }
                 }
+
+                if isTextFieldVisible{
+                    CommentTextField(commentText: $commentText, isTextFieldVisible: $isTextFieldVisible, autoFocused: $autoFocused)
+                }else{
+                    BottomBar
+                }
+                
             }
+            
         }
+        
         
     }
 }
 
 
 // MARK: COMPONENTS
+
+extension SinglePostView{
+    private var BottomBar: some View {
+        VStack {
+            
+                    Spacer()  // Push the HStack to the bottom of the ZStack
+                    HStack {
+                        Spacer()
+                        HStack{
+                            SinglePostLikeBtn(likeState: $postLikeState, heartScale: $heartScale, numLikeState: $postNumLikeState)
+                            Text("\(postNumLikeState)")
+                        }
+                        // Pushes the two buttons apart
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        HStack{
+                            SinglePostCommentBtn(isTextFieldVisible: $isTextFieldVisible, commentText: $commentText, autoFocused: $autoFocused)
+                            Text("\(postNumComments)")
+                        }
+                        Spacer()
+                    }
+                    .background(Color.white)
+                    .shadow(radius: 1)
+                }
+                .edgesIgnoringSafeArea(.bottom)
+    }
+    
+}
+
+struct SinglePostLikeBtn: View{
+        @Binding var likeState: Bool
+        @Binding var heartScale: CGFloat
+        @Binding var numLikeState: Int
+        
+        var body: some View{
+            Button {
+                withAnimation {
+                    // Slightly increase the size for a moment
+                    heartScale = 1.5
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                    withAnimation {
+                        heartScale = 1.0 // Return to normal size
+                    }
+                }
+                toggleLikes()
+                
+            } label: {
+                Image(systemName: likeState ? "heart.fill" : "heart")
+                    .resizable()
+                    .frame(width: 30, height: 25)
+                    .scaleEffect(heartScale)
+                    .foregroundColor(likeState ? .red : .black)
+                    .padding(.vertical)
+                    
+                
+            }.buttonStyle(PlainButtonStyle())
+        }
+        
+        // Toggle number of likes, +1 / -1
+        func toggleLikes(){
+            likeState.toggle()
+            if likeState {
+                numLikeState += 1
+            }else{
+                numLikeState -= 1
+            }
+        }
+    
+}
+
+struct SinglePostCommentBtn: View {
+    @Binding var isTextFieldVisible: Bool
+    @Binding var commentText: String
+    @FocusState.Binding var autoFocused: Bool
+    
+    var body: some View {
+        Button(action: {
+            // Handle message button action
+            isTextFieldVisible = true
+            autoFocused = true
+        }) {
+            Image(systemName: "ellipsis.message")
+                .resizable()
+                .frame(width: 27, height: 27)
+                .foregroundColor(.black)
+                .padding(.vertical)
+        }
+    }
+}
+
+
+struct CommentTextField: View{
+    @Binding var commentText: String
+    @Binding var isTextFieldVisible: Bool
+    @FocusState.Binding var autoFocused: Bool
+    
+    let lineHeight: CGFloat = 30 // Approximate height for a line of text
+    let maxCharactersPerLine: Int = 55 // Measured
+    @State private var editorHeight: CGFloat
+    
+    init(commentText: Binding<String>, isTextFieldVisible: Binding<Bool>, autoFocused: FocusState<Bool>.Binding) {
+        self._commentText = commentText
+        self._isTextFieldVisible = isTextFieldVisible
+        self._autoFocused = autoFocused
+        
+        // Initialize the height of text editor
+        let calculatedHeight = Self.calculateEditorHeight(value: commentText.wrappedValue, maxCharactersPerLine: maxCharactersPerLine, lineHeight: lineHeight)
+        _editorHeight = State(initialValue: calculatedHeight)
+    }
+
+    
+    
+    var body: some View{
+        VStack {
+            Spacer()
+            HStack {
+                    TextEditor(text: $commentText)
+                        .onChange(of: commentText) { value in
+                            editorHeight = Self.calculateEditorHeight(value: value, maxCharactersPerLine: maxCharactersPerLine, lineHeight: lineHeight)
+                        }
+                        .focused($autoFocused) // This is used to set the focus on the TextField
+                        .frame(height: editorHeight)
+                        .scrollContentBackground(.hidden)
+                        .background(.gray.opacity(0.04))
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 0.5))
+                    .padding()
+                
+                
+                Button {
+                    // Send the comment
+                    commentText = ""
+                    isTextFieldVisible = false
+                    autoFocused = false
+                    print("button clicked")
+                } label: {
+                    Text("Send")
+                        .padding(.all, 10)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                }.padding(.trailing,10)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .padding(.horizontal)
+        }
+    }
+    
+    // Calculate the text editor height
+    static func calculateEditorHeight(value: String, maxCharactersPerLine: Int, lineHeight: CGFloat) -> CGFloat {
+        let numberOfLines = max(value.split(whereSeparator: { $0.isNewline }).count, Int(ceil(Double(value.count) / Double(maxCharactersPerLine))))
+        let calculatedHeight = CGFloat(numberOfLines) * lineHeight
+        // Limit the height to a maximum of 90
+        return min(90, max(lineHeight, calculatedHeight))
+    }
+}
+
+
 struct CommentsSection: View {
-    @Binding var likeStates: [Bool]
+    @Binding var commentLikeStates: [Bool]
     @Binding var heartScale: CGFloat
-    @Binding var numLikeStates: [Int]
+    @Binding var commentNumLikeStates: [Int]
     
     var body: some View {
         VStack(alignment:.leading){
@@ -100,12 +297,13 @@ struct CommentsSection: View {
                 Text("384 Comments")
                     .font(.headline)
                     .fontWeight(.thin)
+                    .padding(.horizontal)
                 Spacer()
             }.padding(.bottom)
             
             ForEach(1..<5) { index in
-                SingleComment(index: index, likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
-                    .padding(.vertical)
+                SingleComment(index: index, commentLikeStates: $commentLikeStates, heartScale: $heartScale, commentNumLikeStates: $commentNumLikeStates)
+                    .padding()
                 Divider()
             }
             
@@ -115,9 +313,9 @@ struct CommentsSection: View {
 
 struct SingleComment: View {
     let index: Int
-    @Binding var likeStates: [Bool]
+    @Binding var commentLikeStates: [Bool]
     @Binding var heartScale: CGFloat
-    @Binding var numLikeStates: [Int]
+    @Binding var commentNumLikeStates: [Int]
     
     var body: some View {
 
@@ -140,8 +338,8 @@ struct SingleComment: View {
             }
             // End section: contains like button and number of likes
             VStack{
-                LikeButton(index: index, likeStates: $likeStates, heartScale: $heartScale, numLikeStates: $numLikeStates)
-                Text("\(numLikeStates[index])")
+                LikeButton(index: index, likeStates: $commentLikeStates, heartScale: $heartScale, numLikeStates: $commentNumLikeStates)
+                Text("\(commentNumLikeStates[index])")
                     .font(.footnote)
                     .foregroundColor(.gray)
             }
