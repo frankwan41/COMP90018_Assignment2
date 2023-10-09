82c32aa39d051141dc2c6f78e40d1e3fdf339ca1
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2023-10-09 16:19:44 +1100

    Implement function of displaying multiple photos in post

--- COMP90018_APP/COMP90018_APP/Views/SinglePostView.swift
+++ COMP90018_APP/COMP90018_APP/Views/SinglePostView.swift
@@ -12,22 +12,21 @@ struct SinglePostView: View {
     @State private var likeStates: [Bool] = Array(repeating: false, count: 5)
     @State private var heartScale: CGFloat = 1.0
     @State private var numLikeStates: [Int] = Array(repeating: 32, count: 20)
-
+    @State private var selectedPhotoIndex = 0
     
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
         NavigationView {
             ScrollView{
-                VStack(spacing: 20){
-                    Image(systemName: "photo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .font(.largeTitle)
+                VStack(alignment: .leading, spacing: 20){
+                    PostPhotoView(selectedPhotoIndex: $selectedPhotoIndex)
                     Text("Titles")
                         .font(.title)
                         .fontWeight(.bold)
+                        .padding(.horizontal)
                     Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam. Ut lectus arcu bibendum at varius vel pharetra. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Sed odio morbi quis commodo odio. Pharetra convallis posuere morbi leo urna molestie at. Nisl tincidunt eget nullam non nisi est. Nibh praesent tristique magna sit amet. Sed faucibus turpis in eu mi bibendum neque egestas congue. In arcu cursus euismod quis viverra nibh cras. Tincidunt praesent semper feugiat nibh sed. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Tristique magna sit amet purus.")
+                        .padding(.horizontal)
                     HStack{
                         Text("TimeStamp")
                             .font(.subheadline)
@@ -151,6 +150,35 @@ struct SingleComment: View {
     }
 }
 
+struct PostPhotoView: View {
+    @Binding var selectedPhotoIndex: Int
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedPhotoIndex) {
+                ForEach(1..<5) {_ in
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                }
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .frame(width: UIScreen.main.bounds.width, height: 300)
+        
+        GeometryReader { geo in
+            HStack(spacing: 8) {
+                ForEach(1..<5, id: \.self) { index in
+                    Circle()
+                        .frame(width: 8, height: 8)
+                        .foregroundColor(selectedPhotoIndex == index-1 ? .pink : .gray)
+                }
+            }
+            .position(x: geo.size.width / 2, y: geo.size.height - 20)  // Adjust y value to position
+        }
+    }
+}
 
 
 struct SinglePostView_Previews: PreviewProvider {
