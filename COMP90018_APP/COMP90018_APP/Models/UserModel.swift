e316e0c06919dabd40cd017e9010d8647b3f94d0
junranLin <junran@student.unimelb.edu.au>
2023-10-09 13:51:58 +1100

    Update UserModel.swift

--- COMP90018_APP/COMP90018_APP/Models/UserModel.swift
+++ COMP90018_APP/COMP90018_APP/Models/UserModel.swift
@@ -8,13 +8,14 @@
 import Foundation
 import Firebase
 
-struct User: Decodable{
+struct User: Decodable {
     var userName: String
     var gender: String
     var email: String
     var profileImageURL: String
     var age: String
     var phoneNumber: String
+    var likedPostsIDs: [String]
     
     init(data: [String: Any]){
         self.userName = data["username"] as? String ?? ""
@@ -23,65 +24,10 @@ struct User: Decodable{
         self.profileImageURL = data["profileimageurl"] as? String ?? ""
         self.age = data["age"] as? String ?? ""
         self.phoneNumber = data["phonenumber"] as? String ?? ""
+        self.likedPostsIDs = data["likedpostsids"] as? [String] ?? [""]
     }
     
 }
 
-class UserViewModel: ObservableObject {
-    @Published var isLoggedIn = false
-    @Published var errorMessage: String = ""
-    
-    init() {
-        isLoggedIn = FirebaseManager.shared.auth.currentUser?.uid == nil
-    }
-    
-    func signInUser(email: String, password: String){
-        FirebaseManager.shared.auth.signIn(withEmail: email, password: password){ result, error in
-            if let error = error {
-                print("Failed to sign in user \(error)")
-                self.errorMessage = "Invalid sign credentials!"
-                return
-            }
-            self.isLoggedIn = true
-            print("Successfully signed in as user: \(result!.user.uid)")
-        }
-    }
-    
-    func signUpUser(email: String, password: String){
-        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) {[self] result, error in
-            if let error = error {
-                print("Failed to sign up user \(error)")
-                self.errorMessage = "Failed to sign up"
-                return
-            }
-            print("Successfully signed up user \(result!.user.uid)")
-            self.signInUser(email: email, password: password)
-            print("After signed up: Successfully signed in as user \(result!.user.uid)")
-        }
-    }
-    
-    func resetPassword(email: String) {
-        FirebaseManager.shared.auth.sendPasswordReset(withEmail: email){error in
-            if let error = error {
-                print("Failed to reset password")
-                print("Some error occured \(error)")
-            }
-        }
-    }
-    
-    func updateEmail(email: String) {
-       
-    }
-    
-    func signOutUser(){
-        isLoggedIn = false
-        do{
-            try FirebaseManager.shared.auth.signOut()
-            print("Successfully signed out")
-        }catch{
-            print("Error signed out: \(error)")
-        }
-    }
-    
-}
+
 
