adebcda2db6d1e8b4aea5dfdb8c45b64e485fe7f
junranLin <87986984+junranLin@users.noreply.github.com>
2023-10-09 13:51:41 +1100

    Update WelcomView.swift

--- COMP90018_APP/COMP90018_APP/Views/WelcomView.swift
+++ COMP90018_APP/COMP90018_APP/Views/WelcomView.swift
@@ -5,30 +5,60 @@
 //  Created by frank w on 14/9/2023.
 //
 
+enum viewPage: String{
+    case welcome = "welcome"
+    case tab = "tab"
+    case shake = "shake"
+}
+
+
 import SwiftUI
 
 struct WelcomView: View {
-    @State private var naviagtePosts: Bool = false
+
+    @AppStorage("viewDisplay") var viewSwitcher = viewPage.welcome
+    
+    init(){
+        viewSwitcher = viewPage.welcome
+    }
     
     
     var body: some View {
-        NavigationView {
-            
+        VStack{
+            if viewSwitcher == viewPage.welcome{
+                welcomeMainView()
+            }else if viewSwitcher == viewPage.tab{
+                TabMainView()
+            }else if viewSwitcher == viewPage.shake{
+                ShakeView()
+            }
+        }
+    }
+    
+}
+
+struct welcomeMainView: View {
+    
+    @AppStorage("viewDisplay") var viewSwitcher = viewPage.welcome
+    
+    @State private var isAnimating = false
+    
+    var body: some View{
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.red, Color.yellow]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+                                    )
             VStack(spacing: 150){
                 Text("Do you know what to eat today?")
                     .font(.largeTitle)
                     .multilineTextAlignment(.center)
                 HStack(spacing: 100){
-                    NavigationLink {
-                        PostsView()
-                    } label: {
-                        Text("posts")
-                    }
-
+                    
                     Button {
                         // Navigate to posts view
-                        naviagtePosts = true
-                        
+                        viewSwitcher = viewPage.tab
                     } label: {
                         Text("YES")
                             .font(.headline)
@@ -37,8 +67,19 @@ struct WelcomView: View {
                             .frame(width: 100, height: 50)
                             .background(RoundedRectangle(cornerRadius: 10).fill(Color.green))
                     }
+                    .scaleEffect(isAnimating ? 1.8 : 1.0) // Apply the scale effect
+                    
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                            isAnimating = true
+                        }
+                        
+                    }
+                    
+                    
                     Button {
                         // Navigate to shake view for pick random restaurant
+                        viewSwitcher = viewPage.shake
                     } label: {
                         Text("NO")
                             .font(.headline)
@@ -47,18 +88,24 @@ struct WelcomView: View {
                             .frame(width: 100, height: 50)
                             .background(RoundedRectangle(cornerRadius: 10).fill(Color.red))
                     }
+                    .scaleEffect(isAnimating ? 1.8 : 1.0) // Apply the scale effect
                     
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                            isAnimating = true
+                        }
+                        
+                    }
                 }
             }
-//            .navigationDestination(isPresented: $naviagtePosts) {
-//                PostsView()
-//            }
-        }
+        }.ignoresSafeArea()
     }
 }
 
 struct WelcomView_Previews: PreviewProvider {
     static var previews: some View {
-        WelcomView()
+        NavigationStack{
+            WelcomView()
+        }
     }
 }
