a24d77ceb7d87bc799e80094671b97ba6a974ca7
terrance2630 <tww2@student.unimelb.edu.au>
2023-10-23 20:30:19 +1100

    keep log in fixed.

--- COMP90018_APP/COMP90018_APP/Models/UserViewModel.swift
+++ COMP90018_APP/COMP90018_APP/Models/UserViewModel.swift
@@ -15,7 +15,8 @@ class UserViewModel: ObservableObject{
     @Published var errorMessage = ""
     
     init(){
-        isLoggedIn = FirebaseManager.shared.auth.currentUser?.uid == nil
+        isLoggedIn = FirebaseManager.shared.auth.currentUser?.uid != nil
+        //isLoggedIn = FirebaseManager.shared.auth.currentUser?.uid == nil 不知道是哪个写的，每次都要重新登录。
     }
     
     
