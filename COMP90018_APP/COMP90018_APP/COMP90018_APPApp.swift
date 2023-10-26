3ff07ec058d1eb7548728bd9837324df53f6c631
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2023-10-26 20:12:26 +1100

    Add swipe screen back

--- COMP90018_APP/COMP90018_APP/COMP90018_APPApp.swift
+++ COMP90018_APP/COMP90018_APP/COMP90018_APPApp.swift
@@ -26,13 +26,13 @@ struct COMP90018_APPApp: App {
 
 // Hide navigation bar without losing swipe back gesture in SwiftUI
 // https://stackoverflow.com/a/60067869
-//extension UINavigationController: UIGestureRecognizerDelegate {
-//    override open func viewDidLoad() {
-//        super.viewDidLoad()
-//        interactivePopGestureRecognizer?.delegate = self
-//    }
-//
-//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        return viewControllers.count > 1
-//    }
-//}
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
