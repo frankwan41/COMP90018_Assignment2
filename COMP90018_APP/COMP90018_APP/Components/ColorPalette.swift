ebffbe619315ae678ac3c9daf5d4840adc3a2159
Bowen Fan <bffa@student.unimelb.edu.au>
2023-11-07 22:32:47 +1100

    Added ColorPalette

---
+++ COMP90018_APP/COMP90018_APP/Components/ColorPalette.swift
@@ -0,0 +1,14 @@
+//
+//  ColorPalette.swift
+//  COMP90018_APP
+//
+//  Created by Bowen Fan on 7/11/2023.
+//
+
+import SwiftUI
+
+let backgroundColor: Color = Color(red:241/255, green:166/255, blue:97/255)
+let primaryColor: Color = Color(red:255/255, green:216/255, blue:169/255)
+let secondaryColor: Color = Color(red:253/255, green:238/255, blue:220/255)
+let lightColor: Color = Color(red:250/255, green:238/255, blue:224/255)
+let darkColor: Color = Color(red:219/255, green:208/255, blue:192/255)
